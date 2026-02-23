e9658d4015d1c3a9f7c36b1a90347dcfecdd7327
Shwz.ssilva <juniorsz.7013@gmail.com>
2026-02-23 16:58:40 -0300

    012/300

--- udemyJava/src/fundamentals/loops_condicionais/For_1.java
+++ udemyJava/src/fundamentals/loops_condicionais/For_1.java
@@ -1,4 +1,14 @@
 package fundamentals.loops_condicionais;
 
 public class For_1 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 20; i++){
+            if(i % 2 == 0){
+                System.out.println(i + " par");
+            } else{
+                System.out.println(i + " impar");
+            }
+        }
+    }
 }
--- udemyJava/src/fundamentals/loops_condicionais/While_1.java
+++ udemyJava/src/fundamentals/loops_condicionais/While_1.java
@@ -1,4 +1,24 @@
 package fundamentals.loops_condicionais;
 
+import java.util.Scanner;
+
 public class While_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valorInput = 0;
+        int soma = 0;
+
+        while(valorInput != -1){
+            System.out.print("Adivinhe o numero: ");
+            valorInput = sc.nextInt();
+            if (valorInput != -1){
+                soma += valorInput;
+            }
+        }
+
+
+        System.out.println("Você acertou!!");
+        System.out.println("Soma dos numeros que você digitou: " + soma);
+        sc.close();
+    }
 }
