a213dec3c3e58f0358245ed4753bed622a85f2c4
Shwz.ssilva <juniorsz.7013@gmail.com>
2026-02-28 13:34:18 -0300

    014/300

--- udemyJava/src/oop/oop_basic6/Main.java
+++ udemyJava/src/oop/oop_basic6/Main.java
@@ -1,7 +1,22 @@
 package oop.oop_basic6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite o nome do produto: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Digite o valor do produto: ");
+        double preco = sc.nextDouble();
+
+        Produto p1 = new Produto(nome, preco);
+
+        System.out.println("Produto: " + p1.getNome());
+        System.out.printf("Preço: %.2f%n", p1.getPreco());
+
+        sc.close();
     }
 }
--- udemyJava/src/oop/oop_basic6/Produto.java
+++ udemyJava/src/oop/oop_basic6/Produto.java
@@ -4,12 +4,23 @@ public class Produto {
     private String nome;
     private double preco;
 
-    public void Produto(String nome, double preco){
+    public Produto(String nome, double preco){
         this.nome = nome;
-        if (preco > 0){
-            this.preco = preco;
-        }
+        setPreco(preco);
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
     }
 
-
+    public void setPreco(double preco) {
+        if(preco > 0){
+            this.preco = preco;
+        }else {System.out.println("Não e possivel colocar um valor negativo");}
+    }
+    public double getPreco() {
+        return preco;
+    }
 }
